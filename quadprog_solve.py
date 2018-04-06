8bd450ee1cff6b1dc0c5615f64b0ee65837485e8
Shishir Kolathaya <shishirkolathaya@gmail.com>
2018-04-06 17:17:05 +0530

    Added quadprog test code. Not working properly currently.

--- quadprog_solve.py
+++ quadprog_solve.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python
+""" This code is for solving QP s for our robots """
+__author__ = "Shishir Kolathaya"
+__status__ = "Testing"
+
+import quadprog
+import numpy
+
 def quadprog_solve_qp(P, q, G=None, h=None, A=None, b=None):
     qp_G = .5 * (P + P.T)   # make sure P is symmetric
     qp_a = -q
@@ -10,3 +18,11 @@ def quadprog_solve_qp(P, q, G=None, h=None, A=None, b=None):
         qp_b = -h
         meq = 0
     return quadprog.solve_qp(qp_G, qp_a, qp_C, qp_b, meq)[0]
+
+if __name__ == '__main__':
+    A = 2
+    b = 2
+    P = 2
+    q = 0
+
+    quadprog_solve_qp(P=P,q=q,A=A,b=b)
