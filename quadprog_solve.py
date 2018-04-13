e8315c53df166f04639459483ee97875e358e2db
Shishir Kolathaya <shishirkolathaya@gmail.com>
2018-04-13 12:56:15 +0530

    Working quadprog qp python code from ashish

--- quadprog_solve.py
+++ quadprog_solve.py
@@ -1,4 +1,10 @@
+import time
+import serial
+import subprocess
+ser = serial.Serial('/dev/ttyACM0')
 
+ser.baudrate = 9600
+ser.isOpen()
 
 #!/usr/bin/env python
 """ This code is for solving QP s for our robots """
@@ -29,18 +35,18 @@ def quadprog_solve_qp(H, h, A=None, b=None, C=None, d=None):
     return quadprog.solve_qp(qp_H, qp_h, qp_C, qp_d, meq)[0]
 
 def simple_pd(q_act,q_dot_act):
-    q_des = 0
-    u_in = 10*(q_des - q_act)
+    q_des = 0.
+    u_in = 5*(q_des - q_act)
     return u_in
 
 def qp_q_dot_des(q_act):
-    q_des = 0
+    q_des = 0.
 
     H = array([[1.]])  # cost function matrix is given here   e.g. u^T H u
     h = array([0.])  # cost function vector    e.g. h^T u
 
-    A = array([[-2. * (q_act - q_des)]]) # inequality constraints are given here Au \leq b
-    b = array([- (q_act - q_des)^2])
+    A = array([[(q_act - q_des)]]) # inequality constraints are given here Au \leq b
+    b = array([-100. * (q_act - q_des)*(q_act - q_des)])
 
     u_in = quadprog_solve_qp(H, h, A, b)
 
@@ -59,7 +65,7 @@ if __name__ == '__main__':
     #V = (x_act - x_des)*P*(x_act - x_des)
 
     # mass =  3.  # this is the mass of the lever arm in kilograms
-    k_p = 10.  # proportional gain
+    '''k_p = 10.  # proportional gain
     k_d = 1.   # derivative gain
 
     H = array([[1.]]) # cost function matrix is given here   e.g. u^T H u
@@ -68,5 +74,23 @@ if __name__ == '__main__':
     A = array([[2.*(q_dot_act - q_dot_des)]])
     b = array([-2.*(q_act - q_des)*k_p*q_dot_act - q_dot_act*k_d*q_dot_act])
 
-    result = quadprog_solve_qp(H, h, A, b)
-    print result
+    result = quadprog_solve_qp(H, h, A, b)'''
+    u_in=0
+    while 1:
+        #subprocess.call(["tail", "-n","1","/dev/ttyACM0"])
+        #subprocess.call(["sed","-n","2p","/dev/ttyACM0"])
+        q_act=float(ser.readline())
+        #u_in=simple_pd(q_act,0.)
+        u_in=qp_q_dot_des(q_act)
+	#u_in = 12
+	ser.write("a");
+	if u_in<10 and u_in>-10:
+            ser.write(str(0))
+        ser.write(str(int(u_in[0])))
+        #ser.write("-15")
+        #ser.write("\n")
+	#print str(int(u_in))
+        #ser.write(str(10))
+        print u_in[0];
+        #time.sleep(1)
+        #time.sleep(0.01)
